41033bd6ae719323d1e9ac8974f7cf5f20d35cbd
laura <laurabranc94@gmail.com>
2022-10-27 12:59:26 +0100

    Add notes api endpoint

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 
+const fs = require('fs');
+
 const app = express();
 
 const serverPort = process.env.PORT || 5501;
 
 app.use(express.static('public'));
 
+app.get('/api/notes', async(req, res) => {
+    const dbJSON = JSON.parse(await fs.promises.readFile('db.json'));
+    return res.json(dbJSON);
+})
+
 app.use('/notes', express.static('public/notes.html'));
 app.use('*', express.static('public/index.html'));
+
+
+
 
 app.listen(serverPort, () => {
     console.log(`Server listening on port ${serverPort}`);
