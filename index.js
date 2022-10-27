120e81eeb6da57b0f4427df18d10cfd5092b996a
laura <laurabranc94@gmail.com>
2022-10-27 13:06:39 +0100

    Add post call to store notes

--- index.js
+++ index.js
@@ -13,11 +13,15 @@ app.get('/api/notes', async(req, res) => {
     return res.json(dbJSON);
 })
 
+app.post('/api/notes', async(req, res) => {
+    const {text, title} = req.body;
+    const dbJSON = JSON.parse(await fs.promises.readFile('db.json'));
+    const newNotes = [...dbJSON, {text, title}];
+    await fs.promises.writeFile('db.json', JSON.stringify(newNotes));
+});
+
 app.use('/notes', express.static('public/notes.html'));
 app.use('*', express.static('public/index.html'));
-
-
-
 
 app.listen(serverPort, () => {
     console.log(`Server listening on port ${serverPort}`);
