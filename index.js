1aaad9be6591aa56b51107347d76a96872459f2b
laura <laurabranc94@gmail.com>
2022-10-27 13:15:42 +0100

    Use help for loading json

--- index.js
+++ index.js
@@ -10,21 +10,23 @@ const serverPort = process.env.PORT || 5501;
 
 app.use(express.static('public'));
 
+const getJSON = async() => JSON.parse(await fs.promises.readFile('db.json'));
+
 app.get('/api/notes', async(req, res) => {
-    const dbJSON = JSON.parse(await fs.promises.readFile('db.json'));
+    const dbJSON = await getJSON();
     return res.json(dbJSON);
 })
 
 app.post('/api/notes', async(req, res) => {
     const {text, title} = req.body;
-    const dbJSON = JSON.parse(await fs.promises.readFile('db.json'));
+    const dbJSON =  await getJSON();
     const newNotes = [...dbJSON, {text, title, id: uuid()}];
     await fs.promises.writeFile('db.json', JSON.stringify(newNotes));
 });
 
 app.delete('/api/notes/:id', async(req, res) => {
     const {id} = req.params;
-    const dbJSON = JSON.parse(await fs.promises.readFile('db.json'));
+    const dbJSON = await getJSON();
     const newNotes = dbJSON.filter((note) => note.id !== id);
     await fs.promises.writeFile('db.json', JSON.stringify(newNotes));
 });
